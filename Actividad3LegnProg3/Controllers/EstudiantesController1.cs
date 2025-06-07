4436e09bd80191f6b45fbd8a94a382ca0a23fea1
alexis0314 <alexismercedes365@gmail.com>
2025-06-07 18:13:20 -0400

    Creacion del proyecto

---
+++ Actividad3LegnProg3/Controllers/EstudiantesController1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Actividad3LegnProg3.Controllers
+{
+    public class EstudiantesController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Registro()
+        {
+            return View();
+        }
+    }
+}
