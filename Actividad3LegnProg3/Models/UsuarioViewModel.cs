3d3cceefc8357e2db95b15fc295bf2b9bdb75b9d
alexis0314 <alexismercedes365@gmail.com>
2025-06-07 19:04:57 -0400

    Clase

---
+++ Actividad3LegnProg3/Models/UsuarioViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Actividad3LegnProg3.Models
+{
+    public class UsuarioViewModel
+    {
+        public string Nombrecompleto { get; set; }
+        public string Matricula { get; set; }
+        public string Carrera { get; set; }
+        public string CorreoInsttitucional { get; set; }
+        public string Telefono { get; set; }
+        public string Fechadenaciomento { get; set; }
+        public string Genero { get; set; }
+        public string Turno { get; set; }
+        public string Tipodeingreso { get; set; }
+        public string Estabecado { get; set; }
+        public string Porcentajedebeca { get; set; }
+        public string Terminoycondiciones { get; set; }
+    }
+}
