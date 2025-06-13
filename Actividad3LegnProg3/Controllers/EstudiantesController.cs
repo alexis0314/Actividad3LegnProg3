bd9f96005fa6e38c4b3067b946af3913ffa833e1
alexis0314 <alexismercedes365@gmail.com>
2025-06-13 15:39:03 -0400

    .

--- Actividad3LegnProg3/Controllers/EstudiantesController.cs
+++ Actividad3LegnProg3/Controllers/EstudiantesController.cs
@@ -1,26 +1,49 @@
 ﻿using Actividad3LegnProg3.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Win32;
+using System.Diagnostics.CodeAnalysis;
 using System.Diagnostics.Metrics;
 
 namespace Actividad3LegnProg3.Controllers
 {
     public class EstudiantesController : Controller
     {
+        private static List<EstudianteViewModel> Estudiante = new List<EstudianteViewModel>()
+        {
+             new EstudianteViewModel()
+        };
+
+        public EstudiantesController()
+        {
+
+        }
+
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
+        [HttpGet]
+        public IActionResult Lista()
+        {
+            var estudiantes = Registro();
+
+            return View();
+        }
+
+
         [HttpGet]
         public IActionResult Registro()
         {
             ViewBag.Carrera=new List<Stream> {};
             return View();
         }
-        [HttpGet]
-        public IActionResult Redistro(EstudianteViewModel model)
+        [HttpPost]
+        public IActionResult Registro(EstudianteViewModel model)
         {
             if(ModelState.IsValid)
             {
+                Estudiante.Add(model);
                 ViewBag.Messege="El Estudiante se ha registrado";
                 return View(model);
             }
